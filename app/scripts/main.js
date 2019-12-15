4d1eed0ecf1faa180c8c7f0121cad638b1b90736
Andrey999K <andrey1234xxxx@mail.ru>
2019-12-15 05:22:00 +0300

    update 3

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,25 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {});
-
+let i = 0;
 function menu() {
   let header = document.getElementsByClassName("header");
   let nav = document.getElementsByClassName("nav__list");
   let menu = document.getElementsByClassName("header__menu");
   let menu_close = document.getElementsByClassName("header__menu-close");
   let header_lang = document.getElementsByClassName("header__lang");
-  if (nav[0].style.display == "none") {
+  if (i == 0) {
     nav[0].style.display = "flex";
     header[0].style.opacity = 0.95;
     header[0].style.background = "#ffffff";
     header[0].style.height = "100vh";
-    menu_close[0].style.display = "block";
-    menu[0].style.display = "none";
+    // menu_close[0].style.display = "block";
+    // menu[0].style.display = "none";
+    menu[0].className = "header__menu-close";
     header_lang[0].style.display = "flex";
+    i++;
   } else {
     nav[0].style.display = "none";
     header[0].style.background = "none";
     header[0].style.height = "19px";
-    menu_close[0].style.display = "none";
-    menu[0].style.display = "block";
+    // menu_close[0].style.display = "none";
+    // menu[0].style.display = "block";
+    menu_close[0].className = "header__menu";
     header_lang[0].style.display = "none";
+    i--;
   }
 }
